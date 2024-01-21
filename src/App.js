f64a41a3fb4e5ea054c7f1918d3a213bfae4e0de
sangeetaraddy <shr.sangeetaraddy@gmail.com>
2024-01-21 13:07:35 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Home Page</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to my application</h1>
+        Assignment:creating basic react application
+        Sangeeta:01/21/2024
+      </header>
+    </div>
+  );
+}
+
+export default App;
